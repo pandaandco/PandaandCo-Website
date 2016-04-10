750473c31657a67ce9940e9775293a1bf9d5d4f0
pandaandco <pandaandcocardiff@gmail.com>
2016-04-10 17:31:14 +0100

    Update wsgi.py

--- thesite/wsgi.py
+++ thesite/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
+
 
 import os
 
@@ -14,3 +14,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thesite.settings")
 
 application = get_wsgi_application()
+"""
+import os
+import sys
+
+path = '/home/pandaandcocardiff/PandaandCo-Website'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'thesite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
