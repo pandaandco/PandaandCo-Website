a7c333b28ceb1bdb265ce253481e035d7f54e1e5
pandaandco <pandaandcocardiff@gmail.com>
2016-04-12 17:53:44 +0100

    Update views.py

--- thesite/website/views.py
+++ thesite/website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 def post_list(request):
     return render(request, 'website/post_list.html', {})
+
+def index(request):
+    return HttpResponse("<h2>HEY!</h2>")
